9ffe87b4a8db4515529ae76de80d892dea0274cd
Julien Tourille <julien@latribu.org>
2017-06-30 10:40:01 +0200

    Changing project name in files

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-setup(name='yaner',
+setup(name='yaset',
       version='0.1',
-      description='Yet Another Named Entity Recognizer',
-      url='https://github.com/jtourille/yaner',
+      description='Yet Another SEquence Tagger',
+      url='https://github.com/jtourille/yaset',
       author='Julien Tourille',
       author_email='julien.tourille@limsi.fr',
       license='MIT',
-      packages=['yaner'],
+      packages=['yaset'],
       install_requires=[
           'scikit_learn',
       ],
-      scripts=['bin/yaner'],
+      scripts=['bin/yaset'],
       zip_safe=False)
