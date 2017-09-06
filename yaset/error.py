6df8660ee1b2f9aabc4b1fd3098be17ae89808f3
Julien Tourille <julien@latribu.org>
2017-09-06 21:41:41 +0200

    Add error module

---
+++ yaset/error.py
@@ -0,0 +1,10 @@
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class UnknownTokenAlreadyExists(Error):
+
+    def __init__(self, message):
+        self.message = message
