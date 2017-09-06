fca12e334e427a8a03a8dab2bca46f787ee10f25
Julien Tourille <julien@latribu.org>
2017-09-06 16:18:09 +0200

    Add logging

--- yaset/embed/gensim.py
+++ yaset/embed/gensim.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import os
 
@@ -30,16 +31,20 @@ class GensimEmbeddings:
         """
 
         # Loading gensim object
+        logging.debug("-> Loading gensim file")
         gensim_obj = gensim.models.Word2Vec.load(self.embedding_file_path)
 
         # Copying gensim object embedding matrix
+        logging.debug("-> Fetching embedding matrix from gensim model")
         self.embedding_matrix = gensim_obj.wv.syn0
 
         # Creating token-id mapping
+        logging.debug("-> Creating word-id mapping")
         for i, item in enumerate(gensim_obj.wv.index2word):
             self.word_mapping[item] = i
 
         # Fetching word count from gensim object
+        logging.debug("-> Fetching word count from gensim model")
         for item in gensim_obj.wv.vocab:
             self.word_count[item] = gensim_obj.wv.vocab[item].count
 
@@ -57,14 +62,18 @@ class GensimEmbeddings:
             raise UnknownTokenAlreadyExists("The unknown token already exists")
 
         # Number of token to consider to compute the unknown token vector (1% of the vocabulary size)
+        logging.debug("-> Computing number of vectors to use for averaging and creating empty matrix")
         nb_token_id_to_keep = math.ceil(len(self.word_count) * 0.01)
 
         # Creating an empty temporary matrix
         temp_matrix = np.empty((nb_token_id_to_keep, self.embedding_matrix.shape[1]))
 
         # List of 'n' least frequent tokens
+        logging.debug("-> Fetching token ids")
         least_frequent_tokens = [k for k, f in sorted(self.word_count.items(), reverse=True)][:nb_token_id_to_keep]
         least_frequent_token_ids = [self.word_mapping[k] for k in least_frequent_tokens]
+
+        logging.debug("-> Averaging over {} vectors".format(len(least_frequent_token_ids)))
 
         # Populating the matrix
         for i, k in enumerate(least_frequent_token_ids):
@@ -74,6 +83,7 @@ class GensimEmbeddings:
         unknown_vector = np.mean(temp_matrix, axis=0)
 
         # Appending the unknown token vector to the embedding matrix
+        logging.debug("-> Appending the vector to the matrix and adding a mapping")
         self.embedding_matrix = np.append(self.embedding_matrix,
                                           unknown_vector.reshape(1, self.embedding_matrix.shape[1]),
                                           axis=0)
