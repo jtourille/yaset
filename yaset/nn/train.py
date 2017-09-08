bd6a9b9e1400931443b160ad4a8a15bb75bfd0c8
Julien Tourille <julien@latribu.org>
2017-09-08 19:28:32 +0200

    Add comments + dev performance computation

--- yaset/nn/train.py
+++ yaset/nn/train.py
@@ -3,8 +3,10 @@ import math
 import os
 from collections import defaultdict
 
+import numpy as np
 import tensorflow as tf
 
+from .helpers import TrainLogger
 from .models.lstm import BiLSTMCRF
 
 
@@ -137,23 +139,28 @@ def train_model(working_dir, embedding_object, data_object, train_config):
     config_tf.inter_op_parallelism_threads = train_config["cpu_cores"]
 
     logging.info("Building computation graph")
-    logging.debug("-> Resetting TensorFlow graph")
+
     # Clearing TensorFlow computation graph
+    logging.debug("-> Resetting TensorFlow graph")
     tf.reset_default_graph()
 
-    logging.debug("-> Creating coordinator")
     # Creating TensorFlow thread coordinator
+    logging.debug("-> Creating coordinator")
     coord = tf.train.Coordinator()
 
+    # Computing bucket boundaries for bucketing
     logging.debug("-> Computing bucket boundaries for train instances")
     train_bucket_boundaries = compute_bucket_boundaries(data_object.length_train_instances, train_config["batch_size"])
 
+    # Fetching 'train' and 'dev' instance counts
     train_nb_examples = data_object.nb_train_instances
     dev_nb_examples = data_object.nb_dev_instances
 
+    # Computing TFRecords file paths
     tfrecords_train_file_path = os.path.join(os.path.abspath(working_dir), "tfrecords", "train.tfrecords")
     tfrecords_dev_file_path = os.path.join(os.path.abspath(working_dir), "tfrecords", "dev.tfrecords")
 
+    # Building 'train' input pipeline sub-graph
     logging.debug("-> Building train input pipeline")
     queue_runner_list_train, queue_list_train,\
         batch_train = _build_train_pipeline(tfrecords_train_file_path,
@@ -162,6 +169,7 @@ def train_model(working_dir, embedding_object, data_object, train_config):
                                             batch_size=train_config["batch_size"],
                                             nb_instances=train_nb_examples)
 
+    # Building 'dev' input pipeline sub-graph
     logging.debug("-> Building dev input pipeline")
     queue_runner_list_dev, queue_list_dev,\
         batch_dev = _build_dev_pipeline(tfrecords_dev_file_path,
@@ -169,6 +177,7 @@ def train_model(working_dir, embedding_object, data_object, train_config):
                                         batch_size=train_config["batch_size"],
                                         nb_instances=dev_nb_examples)
 
+    # Network parameters for **kwargs usage
     model_args = {
         "word_embedding_matrix_shape": embedding_object.embedding_matrix.shape,
         "pl_dropout": tf.placeholder(tf.float32),
@@ -178,60 +187,140 @@ def train_model(working_dir, embedding_object, data_object, train_config):
         "output_size": 3
     }
 
-    logging.debug("-> Instantiating NN model")
+    # Creating main computation sub-graph
+    logging.debug("-> Instantiating NN model (train and dev)")
     with tf.name_scope('train'):
         model_train = BiLSTMCRF(batch_train, reuse=False, **model_args)
 
+    # Creating dev computation sub-graph, setting reuse to 'true' for weight sharing
     with tf.name_scope('dev'):
         model_dev = BiLSTMCRF(batch_dev, reuse=True, **model_args)
 
+    # Initialization Op
     with tf.device('/cpu:0'):
         init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
+    # Creating TensorFlow Session object
     logging.debug("-> Creating TensorFlow session and initializing graph")
     sess = tf.Session(config=config_tf)
 
+    # Initializing variables and embedding matrix
     sess.run(init)
     sess.run(model_train.embedding_tokens_init, {model_args["pl_emb"]: embedding_object.embedding_matrix})
 
+    # Launching threads and starting TensorFlow queue runners
     logging.debug("-> Launching threads")
     threads_train = [item.create_threads(sess, coord=coord, start=True) for item in queue_runner_list_train]
     threads_dev = [item.create_threads(sess, coord=coord, start=True) for item in queue_runner_list_dev]
 
     _ = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-    params = {
-        model_args["pl_dropout"]: 0.5
-    }
+    # Computing the 5% threeshold for logging
+    display_every_n_train = math.ceil((train_nb_examples //
+                                       train_config["batch_size"]) * 0.05) * train_config["batch_size"]
 
-    display_every_n = math.ceil((train_nb_examples // train_config["batch_size"]) * 0.05) * train_config["batch_size"]
+    display_every_n_dev = math.ceil((dev_nb_examples //
+                                     train_config["batch_size"]) * 0.05) * train_config["batch_size"]
 
     logging.info("Learning !")
 
     iteration_number = 1
-    counter = 0
+    train_counter = 0
 
-    while iteration_number < train_config["max_iterations"]:
+    train_logger = TrainLogger()
 
-        if counter >= train_nb_examples:
-            counter = 0
+    # Looping until max iteration is reached
+    while iteration_number <= train_config["max_iterations"]:
 
+        # Resetting the counter if an iteration has been completed
+        if train_counter >= train_nb_examples:
+            train_counter = 0
+            iteration_number += 1
+
+        # Starting evaluation on dev corpus if an iteration has been completed
+        if iteration_number - 1 not in train_logger and iteration_number - 1 != 0:
+
+            logging.info("End iteration {}".format(iteration_number - 1))
+            logging.info("Evaluating on dev corpus")
+
+            params = {
+                model_args["pl_dropout"]: 1.0
+            }
+
+            dev_counter = 0
+
+            labels_corr = 0
+            labels_pred = 0
+
+            done = set()
+
+            while dev_counter < dev_nb_examples:
+
+                x_id, x_len, y_pred, y_target = sess.run([batch_dev[0], batch_dev[1], model_dev.prediction,
+                                                          batch_dev[3]], feed_dict=params)
+
+                dev_counter += train_config["batch_size"]
+                cur_percentage = (float(dev_counter) / dev_nb_examples) * 100
+
+                for seq_id_, seq_len_, unary_scores_, y_target_ in zip(x_id, x_len, y_pred, y_target):
+
+                    seq_id_str = seq_id_.decode("UTF-8")
+
+                    if seq_id_str in done:
+                        continue
+                    else:
+                        done.add(seq_id_str)
+
+                    # Decoding with Viterbi
+                    unary_scores_ = unary_scores_[:seq_len_]
+                    viterbi_sequence,\
+                        viterbi_score = tf.contrib.crf.viterbi_decode(unary_scores_,
+                                                                      sess.run(model_dev.transitions_params))
+
+                    # Counting incorrect and correct predictions
+                    for label_pred, label_gs in zip(viterbi_sequence, y_target_):
+                        target_id = np.argmax(label_gs)
+
+                        labels_pred += 1
+                        if data_object.label_mapping[target_id] == data_object.label_mapping[label_pred]:
+                            labels_corr += 1
+
+                # Logging progress
+                if dev_counter % display_every_n_dev == 0 or cur_percentage >= 100:
+                    logging.info("* processed={} ({:5.2f}%)".format(
+                        dev_counter,
+                        round(cur_percentage, 2),
+                    ))
+
+            # Computing token accuracy
+            accuracy = float(labels_corr) / labels_pred
+            logging.info("Accuracy: {}".format(accuracy))
+
+            # Adding iteration score to train logger object
+            train_logger.add_score(iteration_number - 1, accuracy)
+
+        # Setting dropout to 0.5 for learning
+        params = {
+            model_args["pl_dropout"]: train_config["dropout_rate"]
+        }
+
+        # Optimizing with one mini-batch
         _, loss = sess.run([model_train.optimize, model_train.loss], feed_dict=params)
-        counter += 256
-        cur_percentage = (float(counter) / train_nb_examples) * 100
 
-        if counter % display_every_n == 0 or cur_percentage >= 100:
+        # Incrementing counter and computing completion
+        train_counter += 256
+        cur_percentage = (float(train_counter) / train_nb_examples) * 100
 
-            logging.info("* epoch={} ({:05.2f}%), loss={:2d.4f}, processed={}".format(
+        # Logging training progress
+        if train_counter % display_every_n_train == 0 or cur_percentage >= 100:
+            logging.info("* epoch={} ({:5.2f}%), loss={:7.4f}, processed={}".format(
                 iteration_number,
                 round(cur_percentage, 2),
                 loss,
-                counter
+                train_counter
             ))
 
-        if counter >= train_nb_examples:
-            iteration_number += 1
-
+    # Stopping everything gracefully
     logging.info("Stopping everything gracefully (or at least trying to)")
 
     coord.request_stop()
