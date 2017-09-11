fd5d3e8bdc46d9104ac2629773312a5a25652812
Julien Tourille <julien@latribu.org>
2017-09-11 15:40:32 +0200

    Object restructuration -> add a level

--- yaset/nn/helpers.py
+++ yaset/nn/helpers.py
@@ -8,38 +8,67 @@ class TrainLogger:
 
     def __init__(self):
 
-        self.scores = dict()
+        self.iterations_log = dict()
 
-    def __contains__(self, item):
+    def __contains__(self, iteration_number):
+        """
+        Check if an iteration is present in the object
+        :param iteration_number: iteration number
+        :return: boolean
+        """
 
-        return item in self.scores
+        return iteration_number in self.iterations_log
 
     def get_best_iteration(self):
+        """
+        Return the iteration ID of the best iteration
+        :return: iteration ID (int)
+        """
 
         best_iteration = None
         best_score = None
 
-        for ite, payload in self.scores.items():
-            if payload["score"] > best_score:
+        for ite, payload in self.iterations_log.items():
+            if payload["dev_score"] > best_score or best_score is None:
                 best_iteration = ite
+                best_score = payload["dev_score"]
 
         return best_iteration
 
-    def add_score(self, ite, score):
-
-        self.scores[ite] = {
-            "score": score
+    def add_iteration_score(self, ite, score):
+        """
+        Add accuracy score for specific iteration
+        :param ite: iteration number
+        :param score: accuracy score
+        :return: nothing
+        """
+        self.iterations_log[ite] = {
+            "dev_score": score
         }
 
     def save_to_file(self, filename):
+        """
+        Save logger information to file (json format)
+        :param filename: file where to dump information
+        :return: nothing
+        """
 
-        json.dump(self.scores, open(os.path.abspath(filename), "w", encoding="UTF-8"))
+        payload = {
+            "iterations": self.iterations_log
+        }
+
+        json.dump(payload, open(os.path.abspath(filename), "w", encoding="UTF-8"))
 
     def check_patience(self, patience):
+        """
+        Check if patience is reached for current training phase
+        :param patience: patience parameter defined by user
+        :return: boolean (True is patience is reached)
+        """
 
-        current_iteration = len(self.scores) - 1
+        current_iteration = len(self.iterations_log) - 1
 
-        score_list = [i["score"] for k, i in sorted(self.scores.items())]
+        score_list = [ite["dev_score"] for ite_nb, ite in sorted(self.iterations_log.items())]
         score_max = max(score_list)
 
         best_iteration = score_list.index(score_max)
@@ -50,24 +79,32 @@ class TrainLogger:
             return False
 
     def get_score_table(self):
+        """
+        Return iteration performance on 'dev' part formatted as an ASCII table (prettytable object)
+        :return: prettytable object
+        """
 
         x = PrettyTable()
 
-        for iter_nb, payload in sorted(self.scores.items()):
-            x.field_names = ["Iteration", "acc."]
+        for iter_nb, payload in sorted(self.iterations_log.items()):
+            x.field_names = ["Ite. nb.", "dev acc."]
 
             current_iter_nb = "{:3d}".format(iter_nb)
-            current_score = "{:.3f}".format(payload["score"])
+            current_score = "{:.3f}".format(payload["dev_score"])
 
             x.add_row([current_iter_nb, current_score])
 
         return x
 
     def get_removable_iterations(self):
+        """
+        Get a list of least performing iteration for file removing (and disk space preserving)
+        :return: list of iteration numbers
+        """
 
-        score_list = [i["score"] for k, i in sorted(self.scores.items())]
+        score_list = [ite["score"] for ite_nb, ite in sorted(self.iterations_log.items())]
         score_max = max(score_list)
 
         best_iteration = score_list.index(score_max) + 1
 
-        return [i for i in sorted(self.scores) if i != best_iteration]
+        return [i for i in sorted(self.iterations_log) if i != best_iteration]
