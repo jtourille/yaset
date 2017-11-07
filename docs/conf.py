4053d6cb736efbf17902967b621e9dde26b9885b
Julien Tourille <julien@latribu.org>
2017-11-07 20:19:15 +0100

    Add html title

--- docs/conf.py
+++ docs/conf.py
@@ -99,6 +99,8 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #
 # html_theme_options = {}
 
+html_title = "YASET documentation"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
