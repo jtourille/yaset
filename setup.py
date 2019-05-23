434a973dc3011178c4d18f37685183e71f79c553
Julien Tourille <julien@backward.io>
2019-05-23 16:15:29 +0200

    Update setup classifiers

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
     scripts=['bin/yaset']
 )
