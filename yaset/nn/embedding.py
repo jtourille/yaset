72f7cd9f96b847da8f038983b3664b2eff3212e2
Julien Tourille <julien@backward.io>
2019-05-29 15:08:34 +0200

    Allow bert embedding finetuning

--- yaset/nn/embedding.py
+++ yaset/nn/embedding.py
@@ -60,8 +60,10 @@ class Embedder(nn.Module):
             self.bert_embedding = BertEmbeddings(
                 model_name=self.embeddings_options.get("bert").get("model_path"),
                 do_lower_case=self.embeddings_options.get("bert").get("do_lower_case"),
-                vocab_file=self.embeddings_options.get("bert").get("vocab_file"))
-            self.embedding_size += 768
+                vocab_file=self.embeddings_options.get("bert").get("vocab_file"),
+                fine_tune=self.embeddings_options.get("bert").get("fine_tune")
+            )
+            self.embedding_size += self.bert_embedding.hidden_size
 
     def forward(self, batch, cuda):
 
@@ -94,97 +96,30 @@ class BertEmbeddings(nn.Module):
     def __init__(self,
                  model_name: str = None,
                  do_lower_case: bool = None,
-                 vocab_file: str = None):
+                 vocab_file: str = None,
+                 fine_tune: bool = False):
         super().__init__()
 
         self.model_name = model_name
         self.do_lower_case = do_lower_case
         self.vocab_file = vocab_file
-        self.weights = nn.Parameter(torch.randn(12), requires_grad=True)
+        self.fine_tune = fine_tune
 
         self.tokenizer = BertTokenizer.from_pretrained(self.vocab_file, do_lower_case=self.do_lower_case)
         self.model = BertModel.from_pretrained(self.model_name)
 
+        self.weights = nn.Parameter(torch.randn(self.model.config.num_hidden_layers), requires_grad=True)
+
+        self.hidden_size = self.model.config.hidden_size
+
     def forward(self, sequence_list, cuda):
 
-        with torch.no_grad():
+        if self.fine_tune:
+            selected_encoders_layers = self.compute_embeddings(sequence_list, cuda)
 
-            all_token_pieces = list()
-            all_indices = list()
-
-            for sequence in sequence_list:
-                seq_token_pieces = list()
-                seq_indices = list()
-                seq_index = 1
-
-                for token in sequence:
-                    seq_token_pieces.append(self.tokenizer.tokenize(token))
-
-                for token_pieces in seq_token_pieces:
-                    seq_indices.append(seq_index)
-                    seq_index += len(token_pieces)
-
-                seq_token_pieces = [k for l in seq_token_pieces for k in l]
-                seq_token_pieces.insert(0, "[CLS]")
-                seq_token_pieces.append("[SEP]")
-
-                all_token_pieces.append(seq_token_pieces)
-                all_indices.append(seq_indices)
-
-            max_seq_len_pieces = max([len(seq) for seq in all_token_pieces])
-            max_seq_len_str = max([len(seq) for seq in sequence_list])
-
-            tensor_all_input_ids = list()
-            tensor_all_input_type_ids = list()
-            tensor_all_input_mask = list()
-            tensor_all_indices = list()
-
-            for seq_token_pieces, seq_indices in zip(all_token_pieces, all_indices):
-                input_ids = self.tokenizer.convert_tokens_to_ids(seq_token_pieces)
-                input_type_ids = [0] * len(input_ids)
-                input_mask = [1] * len(input_ids)
-
-                while len(input_ids) < max_seq_len_pieces:
-                    input_ids.append(0)
-                    input_type_ids.append(0)
-                    input_mask.append(0)
-
-                tensor_all_input_ids.append(torch.LongTensor([input_ids]))
-                tensor_all_input_type_ids.append(torch.LongTensor([input_type_ids]))
-                tensor_all_input_mask.append(torch.LongTensor([input_mask]))
-                tensor_all_indices.append(torch.LongTensor(seq_indices))
-
-            tensor_all_input_ids = torch.cat(tensor_all_input_ids, 0)
-            tensor_all_input_type_ids = torch.cat(tensor_all_input_type_ids, 0)
-            tensor_all_input_mask = torch.cat(tensor_all_input_mask, 0)
-
-            if cuda:
-                tensor_all_input_ids = tensor_all_input_ids.cuda()
-                tensor_all_input_type_ids = tensor_all_input_type_ids.cuda()
-                tensor_all_input_mask = tensor_all_input_mask.cuda()
-                tensor_all_indices = [item.cuda() for item in tensor_all_indices]
-
-            all_encoders_layers, _ = self.model(
-                tensor_all_input_ids,
-                token_type_ids=tensor_all_input_type_ids,
-                attention_mask=tensor_all_input_mask
-            )
-
-            selected_encoders_layers = list()
-            for layer in all_encoders_layers:
-                selected_layer = list()
-                for a, i in zip(layer, tensor_all_indices):
-                    selected = torch.index_select(a, 0, i)
-
-                    if selected.size(0) < max_seq_len_str:
-                        padding = torch.zeros(max_seq_len_str - selected.size(0), selected.size(1))
-                        if cuda:
-                            padding = padding.cuda()
-                        selected = torch.cat([selected, padding], 0)
-
-                    selected_layer.append(selected.unsqueeze(0))
-                selected_layer = torch.cat(selected_layer, 0)
-                selected_encoders_layers.append(selected_layer)
+        else:
+            with torch.no_grad():
+                selected_encoders_layers = self.compute_embeddings(sequence_list, cuda)
 
         softmaxed_weights = F.softmax(self.weights, dim=0)
         weighted_sum = torch.mul(selected_encoders_layers[0], softmaxed_weights[0])
@@ -193,3 +128,84 @@ class BertEmbeddings(nn.Module):
             weighted_sum = torch.add(weighted_sum, torch.mul(selected_encoders_layers[i], softmaxed_weights[i]))
 
         return weighted_sum
+
+    def compute_embeddings(self, sequence_list, cuda):
+
+        all_token_pieces = list()
+        all_indices = list()
+
+        for sequence in sequence_list:
+            seq_token_pieces = list()
+            seq_indices = list()
+            seq_index = 1
+
+            for token in sequence:
+                seq_token_pieces.append(self.tokenizer.tokenize(token))
+
+            for token_pieces in seq_token_pieces:
+                seq_indices.append(seq_index)
+                seq_index += len(token_pieces)
+
+            seq_token_pieces = [k for l in seq_token_pieces for k in l]
+            seq_token_pieces.insert(0, "[CLS]")
+            seq_token_pieces.append("[SEP]")
+
+            all_token_pieces.append(seq_token_pieces)
+            all_indices.append(seq_indices)
+
+        max_seq_len_pieces = max([len(seq) for seq in all_token_pieces])
+        max_seq_len_str = max([len(seq) for seq in sequence_list])
+
+        tensor_all_input_ids = list()
+        tensor_all_input_type_ids = list()
+        tensor_all_input_mask = list()
+        tensor_all_indices = list()
+
+        for seq_token_pieces, seq_indices in zip(all_token_pieces, all_indices):
+            input_ids = self.tokenizer.convert_tokens_to_ids(seq_token_pieces)
+            input_type_ids = [0] * len(input_ids)
+            input_mask = [1] * len(input_ids)
+
+            while len(input_ids) < max_seq_len_pieces:
+                input_ids.append(0)
+                input_type_ids.append(0)
+                input_mask.append(0)
+
+            tensor_all_input_ids.append(torch.LongTensor([input_ids]))
+            tensor_all_input_type_ids.append(torch.LongTensor([input_type_ids]))
+            tensor_all_input_mask.append(torch.LongTensor([input_mask]))
+            tensor_all_indices.append(torch.LongTensor(seq_indices))
+
+        tensor_all_input_ids = torch.cat(tensor_all_input_ids, 0)
+        tensor_all_input_type_ids = torch.cat(tensor_all_input_type_ids, 0)
+        tensor_all_input_mask = torch.cat(tensor_all_input_mask, 0)
+
+        if cuda:
+            tensor_all_input_ids = tensor_all_input_ids.cuda()
+            tensor_all_input_type_ids = tensor_all_input_type_ids.cuda()
+            tensor_all_input_mask = tensor_all_input_mask.cuda()
+            tensor_all_indices = [item.cuda() for item in tensor_all_indices]
+
+        all_encoders_layers, _ = self.model(
+            tensor_all_input_ids,
+            token_type_ids=tensor_all_input_type_ids,
+            attention_mask=tensor_all_input_mask
+        )
+
+        selected_encoders_layers = list()
+        for layer in all_encoders_layers:
+            selected_layer = list()
+            for a, i in zip(layer, tensor_all_indices):
+                selected = torch.index_select(a, 0, i)
+
+                if selected.size(0) < max_seq_len_str:
+                    padding = torch.zeros(max_seq_len_str - selected.size(0), selected.size(1))
+                    if cuda:
+                        padding = padding.cuda()
+                    selected = torch.cat([selected, padding], 0)
+
+                selected_layer.append(selected.unsqueeze(0))
+            selected_layer = torch.cat(selected_layer, 0)
+            selected_encoders_layers.append(selected_layer)
+
+        return selected_encoders_layers
